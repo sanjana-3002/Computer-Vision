e22227a009115d48101e5b163b94a82588aef93f
Sanjana Waghray <sanjana.waghray3002@gmail.com>
2026-02-24 19:35:24 -0600

    comments

--- learning/read.py
+++ learning/read.py
@@ -4,6 +4,7 @@ import numpy as np
 # Reading images
 img = cv.imread('photos/cat_large.jpg') # this is a larger image hence would be bigger in size and would take more time to process
 # additionally, if we would have processed the cat.jpg, it would have been faster as it is a smaller image.
+# It totally depends on the pixels ( picture image )
 cv.imshow('cat', img)
 cv.waitKey(0)
 
