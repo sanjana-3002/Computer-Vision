646b596b808d5061c9a063549354998e6d457771
Sanjana Waghray <sanjana.waghray3002@gmail.com>
2026-02-24 19:34:52 -0600

    read-videos

--- learning/read.py
+++ learning/read.py
@@ -6,3 +6,5 @@ img = cv.imread('photos/cat_large.jpg') # this is a larger image hence would be 
 # additionally, if we would have processed the cat.jpg, it would have been faster as it is a smaller image.
 cv.imshow('cat', img)
 cv.waitKey(0)
+
+# Reading videos
