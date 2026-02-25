d1cf1558c4c1b81dfb01bd2402c29c876296e4ac
Sanjana Waghray <sanjana.waghray3002@gmail.com>
2026-02-24 19:58:57 -0600

    rescaling

--- learning/rescale.py
+++ learning/rescale.py
@@ -4,5 +4,16 @@ import numpy as np
 # Reading images
 img = cv.imread('photos/cat_large.jpg') # this is a larger image hence would be bigger in size and would take more time to process
 
-cv.imshow('cat', img)
-cv.waitKey(0)
+# Rescaling images
+# we can rescale the image to a smaller size to make it faster to process
+
+def rescaleFrame(frame, scale=0.75):
+    # Images, Videos and Live Video
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
