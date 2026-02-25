5f1dd1655e0809212f9bc776e91aab3e5c58fb91
Sanjana Waghray <sanjana.waghray3002@gmail.com>
2026-02-25 16:48:39 -0600

    rotation

--- learning/transformations.py
+++ learning/transformations.py
@@ -15,3 +15,24 @@ def translate(img, x, y):
 # x --> Right
 # y --> Down
 # it is mainly used to shift an image linearly from one axis to the other
+
+translated = translate(img, -100, 100)
+cv.imshow('Translated', translated)
+
+# Rotation
+def rotate(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width,height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+rotated_rotated = rotate(img, -90)
+cv.imshow('Rotated Rotated', rotated_rotated)
