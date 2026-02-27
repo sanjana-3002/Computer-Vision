38639bda1cdf4a8e49e34713917572e47c6111ee
Sanjana Waghray <sanjana.waghray3002@gmail.com>
2026-02-26 22:16:59 -0600

    fixes

--- learning/autovision-pipeline/preprocessing.py
+++ learning/autovision-pipeline/preprocessing.py
@@ -138,7 +138,7 @@ class ChartPreprocessor:
         axes[1, 2].axis('off')
 
         plt.tight_layout()
-        plt.savefig('data/raw/day1_output.png', dpi=150, bbox_inches='tight')
+        plt.savefig('/Users/sanjanawaghray/Documents/projects/Computer-Vision-1/learning/raw.webp'', dpi=150, bbox_inches='tight')
         plt.show()
         print("\nVisualization saved to data/raw/day1_output.png")
 
@@ -149,7 +149,7 @@ if __name__ == "__main__":
 
     # Use a chart image you downloaded from TradingView
     # Or use any image to start
-    image_path = sys.argv[1] if len(sys.argv) > 1 else "data/raw/chart1.png"
+    image_path = sys.argv[1] if len(sys.argv) > 1 else "/Users/sanjanawaghray/Documents/projects/Computer-Vision-1/learning/raw.webp'"
 
     processor = ChartPreprocessor(image_path)
     processor.explore_as_array()
